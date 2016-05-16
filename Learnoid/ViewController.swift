0f68cc05a7f28f3b4414eb647cdbd34d4d43b9a4
Panosstylianou <panosstylianou@hotmail.com>
2016-05-16 22:57:16 +0100

    Initial Commit

---
+++ Learnoid/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Learnoid
+//
+//  Created by Panayiotis Stylianou on 16/05/2016.
+//  Copyright © 2016 Panayiotis Stylianou. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
